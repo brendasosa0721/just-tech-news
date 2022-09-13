eb16f6fcbb956e457b2c61e04c9fffde0f022ba9
Brenda  Sosa <brenda.sosa@gmail.com>
2022-09-13 18:27:23 -0400

    add the comment route

--- routes/api/routes/api/user-routes.js
+++ routes/api/routes/api/user-routes.js
@@ -84,7 +84,33 @@ router.post("/login", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
+User.findOne({
+  attributes: { exclude: ["password"] },
+  where: {
+    id: req.params.id,
+  },
+  include: [
+    {
+      model: Post,
+      attributes: ["id", "title", "post_url", "created_at"],
+    },
+    // include the Comment model here:
+    {
+      model: Comment,
+      attributes: ["id", "comment_text", "created_at"],
+      include: {
+        model: Post,
+        attributes: ["title"],
+      },
+    },
+    {
+      model: Post,
+      attributes: ["title"],
+      through: Vote,
+      as: "voted_posts",
+    },
+  ],
+});
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
